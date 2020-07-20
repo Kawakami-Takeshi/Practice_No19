0cec87014f34065dd0b5d1518ee1da1ef5b0b5ab
”Kawakami-Takeshi” <take.kawakami12@gmail.com>
2020-07-20 13:48:52 +0000

    practice_19

---
+++ create.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<htm1>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        
+        <title>kawacreate</title>
+    </head>
+    <bode>
+        <h1>クリエイト作成画面</h1>
+    </bode>    
+    
+    
+</htm1>
